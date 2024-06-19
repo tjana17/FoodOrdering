999dacec891fadbf8da3766fe175c97c00db4057
Janarthanan Kannan <tjana17@gmail.com>
2024-06-19 17:05:22 +0530

    Initial Commit

---
+++ FoodOrdering/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodOrdering
+//
+//  Created by Jana's MacBook Pro on 6/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
